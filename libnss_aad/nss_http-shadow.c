ed3599ac202060481e12c3cb1396132c412651b5
Hugo Meiland <humeilan@microsoft.com>
2020-04-12 12:18:24 +0200

    full shadow

--- libnss_aad/nss_http-shadow.c
+++ libnss_aad/nss_http-shadow.c
@@ -11,17 +11,18 @@ static int ent_json_idx = 0;
 // -1 Failed to parse
 // -2 Buffer too small
 static int
-pack_shadow_struct(json_t *root, struct spwd *result, char *buffer, size_t buflen)
+//pack_shadow_struct(json_t *root, struct spwd *result, char *buffer, size_t buflen)
+pack_shadow_struct(json_t *entry_data, struct spwd *result, char *buffer, size_t buflen)
 {
 
     char *next_buf = buffer;
     size_t bufleft = buflen;
 
-    if (!json_is_object(root)) return -1;
-    json_t *passwd_object = json_object_get(root, "value");
-    if (!json_is_array(passwd_object)) return -1;
-    if (json_array_size(passwd_object) < 1) return -1;
-    json_t *entry_data = json_array_get(passwd_object, 0);
+    //if (!json_is_object(root)) return -1;
+    //    json_t *passwd_object = json_object_get(root, "value");
+    //if (!json_is_array(passwd_object)) return -1;
+    //if (json_array_size(passwd_object) < 1) return -1;
+    //json_t *entry_data = json_array_get(passwd_object, 0);
     json_t *extension_object = json_object_get(entry_data, "extj8xolrvw_linux");
 
     json_t *j_sp_namp = json_object_get(extension_object, "user");
@@ -51,17 +52,17 @@ pack_shadow_struct(json_t *root, struct spwd *result, char *buffer, size_t bufle
     next_buf += strlen(result->sp_namp) + 1;
     bufleft  -= strlen(result->sp_namp) + 1;
 
-    if (bufleft <= strlen("x")) return -2;
+    if (bufleft <= strlen("*")) return -2;
     //result->sp_pwdp = strncpy(next_buf, json_string_value(j_sp_pwdp), bufleft);
-    result->sp_pwdp = strncpy(next_buf, "x", bufleft);
+    result->sp_pwdp = strncpy(next_buf, "*", bufleft);
     next_buf += strlen(result->sp_pwdp) + 1;
     bufleft  -= strlen(result->sp_pwdp) + 1;
 
     // Yay, ints are so easy!
-    result->sp_lstchg = 1; //json_integer_value(j_sp_lstchg);
+    result->sp_lstchg = 18364; //json_integer_value(j_sp_lstchg);
     result->sp_min = 0; //json_integer_value(j_sp_min);
     result->sp_max = 99999; //json_integer_value(j_sp_max);
-    result->sp_warn = 0; //json_integer_value(j_sp_warn);
+    result->sp_warn = 7; //json_integer_value(j_sp_warn);
 
     //if (!json_is_null(j_sp_inact)) result->sp_inact = json_integer_value(j_sp_inact);
     result->sp_inact = -1;
@@ -79,31 +80,42 @@ pack_shadow_struct(json_t *root, struct spwd *result, char *buffer, size_t bufle
 enum nss_status
 _nss_aad_setspent_locked(int stayopen)
 {
-    char url[512];
+    char graph_url[512], token_url[512], token_postfield[512], auth_header[2048];
+    const char * access_token;
     json_t *json_root;
     json_error_t json_error;
 
-    printf("setspent_locked\n");
+    char *client_id = nss_read_config("client_id");
+    char *secret = nss_read_config("secret");
+    char *authority = nss_read_config("authority");
 
-    snprintf(url, 512, "http://" NSS_HTTP_SERVER ":" NSS_HTTP_PORT "/shadow");
+    snprintf(token_url, 512, "%s/oauth2/v2.0/token", authority);
+    snprintf(token_postfield, 512, "client_id=%s&scope=https%%3A%%2F%%2Fgraph.microsoft.com%%2F.default&client_secret=%s&grant_type=client_credentials", client_id, secret);
 
-    char *response = nss_http_request(url, NULL);
+    char *token = nss_http_token_request(token_url, token_postfield);
+
+    json_root = json_loads(token, 0, &json_error);
+    json_t *access_token_object = json_object_get(json_root, "access_token");
+    access_token = json_string_value(access_token_object);
+    if (json_is_string(access_token_object)) {
+      snprintf(auth_header, 2048, "%s %s\n", "Authorization: Bearer", access_token);
+    }
+    json_decref(json_root);
+
+    snprintf(graph_url, 512, "https://graph.microsoft.com/v1.0/users?$select=id,extj8xolrvw_linux");
+
+    char *response = nss_http_request(graph_url, auth_header);
+    
     if (!response) {
         return NSS_STATUS_UNAVAIL;
     }
 
     json_root = json_loads(response, 0, &json_error);
+    if (!json_is_array(json_object_get(json_root, "value"))) return -1;
+    if (json_array_size(json_object_get(json_root, "value")) < 1) return -1;
+    json_t *shadow_object = json_object_get(json_root, "value");
 
-    if (!json_root) {
-        return NSS_STATUS_UNAVAIL;
-    }
-
-    if (!json_is_array(json_root)) {
-        json_decref(json_root);
-        return NSS_STATUS_UNAVAIL;
-    }
-
-    ent_json_root = json_root;
+    ent_json_root = shadow_object;
     ent_json_idx = 0;
 
     return NSS_STATUS_SUCCESS;
@@ -114,7 +126,6 @@ _nss_aad_setspent_locked(int stayopen)
 enum nss_status
 _nss_aad_setspent(int stayopen)
 {
-    printf("setspent\n");
     enum nss_status ret;
     NSS_HTTP_LOCK();
     ret = _nss_aad_setspent_locked(stayopen);
@@ -126,7 +137,6 @@ _nss_aad_setspent(int stayopen)
 enum nss_status
 _nss_aad_endspent_locked(void)
 {
-    printf("endspent_locked\n");
     if (ent_json_root){
         while (ent_json_root->refcount > 0) json_decref(ent_json_root);
     }
@@ -140,7 +150,6 @@ _nss_aad_endspent_locked(void)
 enum nss_status
 _nss_aad_endspent(void)
 {
-    printf("endspent\n");
     enum nss_status ret;
     NSS_HTTP_LOCK();
     ret = _nss_aad_endspent_locked();
@@ -152,7 +161,6 @@ _nss_aad_endspent(void)
 enum nss_status
 _nss_aad_getspent_r_locked(struct spwd *result, char *buffer, size_t buflen, int *errnop)
 {
-    printf("getspent_locked\n");
     enum nss_status ret = NSS_STATUS_SUCCESS;
 
     if (ent_json_root == NULL) {
@@ -190,7 +198,6 @@ _nss_aad_getspent_r_locked(struct spwd *result, char *buffer, size_t buflen, int
 enum nss_status
 _nss_aad_getspent_r(struct spwd *result, char *buffer, size_t buflen, int *errnop)
 {
-    printf("getspent\n");
     enum nss_status ret;
     NSS_HTTP_LOCK();
     ret = _nss_aad_getspent_r_locked(result, buffer, buflen, errnop);
@@ -238,8 +245,13 @@ _nss_aad_getspnam_r_locked(const char *name, struct spwd *result, char *buffer, 
         *errnop = ENOENT;
         return NSS_STATUS_UNAVAIL;
     }
+    if (!json_is_array(json_object_get(json_root, "value"))) return -1;
+    if (json_array_size(json_object_get(json_root, "value")) < 1) return -1;
+    json_t *shadow_object = json_object_get(json_root, "value");
+    json_t *entry_data = json_array_get(shadow_object, 0);
 
-    int pack_result = pack_shadow_struct(json_root, result, buffer, buflen);
+    int pack_result = pack_shadow_struct(entry_data, result, buffer, buflen);
+
     if (pack_result == -1) {
         json_decref(json_root);
         *errnop = ENOENT;
