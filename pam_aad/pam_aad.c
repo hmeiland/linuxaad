289282f425b6b6c792d8dae54afaf0660a3e7ca4
Hugo Meiland <humeilan@microsoft.com>
2020-04-12 11:21:24 +0200

    added 2 pam func

--- pam_aad/pam_aad.c
+++ pam_aad/pam_aad.c
@@ -75,3 +75,15 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, cons
     return PAM_AUTHINFO_UNAVAIL;
 }
 
+PAM_EXTERN int pam_sm_setcred(pam_handle_t * pamh, int flags,
+                              int argc, const char **argv)
+{
+    return PAM_SUCCESS;
+}
+
+PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t * pamh, int flags,
+                                int argc, const char **argv)
+{
+    return PAM_SUCCESS;
+}
+
